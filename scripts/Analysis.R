5a844c44b0c97df657e9be3d6b8b8d72313a4179
Levi Yoder Raskin <122696329+Levi-Raskin@users.noreply.github.com>
2025-11-04 13:29:28 -0800

    commits

--- scripts/Analysis.R
+++ scripts/Analysis.R
@@ -1,4 +1,5 @@
 library(geomorph)
+library(geometry)
 landmarks <- read.delim("/Users/levir/Documents/GitHub/HomininTaxicDiversity/results/simulatedShapes/twoDimensionSimTreeIndex6Alpha1.000000Beta1.000000LM25Rate0.300000Dataset0nodeShapes.tsv", header = F)
 cn <- colnames(landmarks)
 cn[1] <- "label"
@@ -17,9 +18,10 @@ for(i in unique(landmarks$label)){
     pcaMat[which(rownames(pcaMat)== i), j] = as.numeric(lm[j]) 
   }
 }
-### gpagen expects an array containing n x d slices where n is num landmarks and d is dimension of ladnmarks
 
 
+
+# Disparity ---------------------------------------------------------------
 subset_mat <- pcaMat[!grepl("^Tip", rownames(pcaMat)), , drop = FALSE]
 tempFullMat <- pcaMat
 disparityPlotMat <- matrix(data = NA, nrow = 160-16, ncol = 2)
@@ -38,3 +40,34 @@ for (i in 1:(160-16)) {
 
 colnames(disparityPlotMat) <- c("num taxa", "disparity")
 plot(disparityPlotMat)
+
+# Chull volume ------------------------------------------------------------
+
+garray <- geomorph::arrayspecs(pcaMat, numLandmarks, k = dimension)
+gpa <- gpagen(garray, print.progress = FALSE, ProcD = TRUE)
+fullSpecimenPCA <- geomorph::gm.prcomp(gpa$coords)
+fullSpecimenPCs <- fullSpecimenPCA$x[,1:3] #just axes 1:3
+
+full_hull <- convhulln(fullSpecimenPCs, options = "FA")
+full_vol <- full_hull$vol
+
+subset_mat <- fullSpecimenPCs[!grepl("^Tip", rownames(fullSpecimenPCs)), , drop = FALSE]
+tempFullMat <- fullSpecimenPCs
+chullPlotMat <- matrix(data = NA, nrow = 160-16, ncol = 2)
+for (i in 1:(160-16)) {
+  tip_rows <- grep("^Tip", rownames(tempFullMat), value = TRUE)
+  sampled_row <- sample(tip_rows, 1)
+  subset_mat <- rbind(subset_mat, tempFullMat[sampled_row, , drop = FALSE])
+  tempFullMat <- tempFullMat[setdiff(rownames(tempFullMat), sampled_row), , drop = FALSE]
+  
+  subset_hull <- convhulln(subset_mat, options = "FA")
+  subset_vol <- subset_hull$vol
+
+    chullPlotMat[i, ] <- c(i + 16, subset_vol)
+}
+
+colnames(chullPlotMat) <- c("num taxa", "subset hull volume")
+plot(chullPlotMat)
+
+
+
